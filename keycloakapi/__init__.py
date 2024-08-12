7efdf6e7d398b370daeb75c4531f7dae94ecdeeb
Patrick Iannini <patrickiannini@knights.ucf.edu>
2024-08-12 13:00:23 -0400

    feature: add KeycloakRoles to init

--- keycloakapi/__init__.py
+++ keycloakapi/__init__.py
@@ -2,5 +2,6 @@ from .utils.auth import KeycloakAuth
 from .realm import KeycloakRealm
 from .client import KeycloakClient
 from .client import ClientConfig
+from .roles import KeycloakRoles
 from .user_federation import KeycloakUserFederation
 from .user_federation import UserFederationConfig
