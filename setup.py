4db90413ebfd05cd9b85881e70cd2f16e309b536
Patrick Iannini <patrickiannini@knights.ucf.edu>
2024-08-26 10:17:47 -0400

    feat: publish 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keycloakapi',
-    version='0.1.0',
+    version='1.0.0',
     description="A Python library for Keycloak's REST API",
     author='Patrick Iannini',
     packages=find_packages(),
