0a91c387551613e550e6032e84e272c0a443d2fe
Patrick Iannini <patrickiannini@knights.ucf.edu>
2024-08-28 09:41:08 -0400

    feat: begin to support testing of multiple keycloak versions

--- keycloakapi/tests/run-tests.sh
+++ keycloakapi/tests/run-tests.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+# Syntax changes for macos
 
-# Start Keycloak container
-docker-compose up -d
+# List of Keycloak versions to test against
+keycloak_versions=("18.0.2" "25.0.2" "latest")
 
-# Wait for Keycloak to be ready
-echo "Waiting for Keycloak to start..."
-sleep 10 
+# Function to wait for Keycloak to be responsive
+wait_for_keycloak() {
+    url=$1
+    echo "Waiting for Keycloak at $url to be responsive..."
+    while ! curl -s $url > /dev/null; do
+        sleep 1
+    done
+}
 
-# Run tests
-pytest
+# Loop through each version
+for version in "${keycloak_versions[@]}"; do
+    echo "Testing against Keycloak version $version"
 
-# Stop Keycloak container
-docker-compose down
+    # Update the docker-compose file to use the specific Keycloak version
+    sed -i "" "s|/keycloak:.*|/keycloak:$version|g" docker-compose.yml
+
+    # Start Keycloak container
+    docker compose up -d
+
+    # Wait for Keycloak to be responsive
+    wait_for_keycloak "http://localhost:8080/"
+
+    # Run tests
+    pytest
+
+    # Stop Keycloak container
+    docker compose down
+done
