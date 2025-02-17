647ea6279d0f6d3fc164011859d4a6317b45f527
Patrick Iannini <patrickiannini@knights.ucf.edu>
2025-02-17 14:39:38 -0500

    feat: support for adding client scope mappers

--- keycloakapi/client_scope.py
+++ keycloakapi/client_scope.py
@@ -50,3 +50,22 @@ class KeycloakClientScope:
             response = requests.put(url, headers=self.auth.get_headers(), json=client_scope_config)
             return response
         
+    def add_client_scope_mapper(self, realm_name, client_scope_name, mapper_config):
+        client_scope = self.get_client_scope(realm_name, client_scope_name)
+        if client_scope:
+            client_scope_id = client_scope["id"]
+            url = f"{self.auth.base_url}/admin/realms/{realm_name}/client-scopes/{client_scope_id}/protocol-mappers/models"
+            response = requests.post(url, headers=self.auth.get_headers(), json=mapper_config)
+            return response
+        
+    def get_client_scope_mapper(self, realm_name, client_scope_name, mapper_name):
+        client_scope = self.get_client_scope(realm_name, client_scope_name)
+        if client_scope:
+            client_scope_id = client_scope["id"]
+            url = f"{self.auth.base_url}/admin/realms/{realm_name}/client-scopes/{client_scope_id}/protocol-mappers/models"
+            response = requests.get(url, headers=self.auth.get_headers())
+            mappers = response.json()
+            for mapper in mappers:
+                if mapper["name"] == mapper_name:
+                    return mapper
+        return None
